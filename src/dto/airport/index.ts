eb102384d3224774fce8e844f59dcdd58fbb37e0
Stephan-MDD <stephan.mdd@gmail.com>
2020-11-15 23:23:31 +0100

    change dto from interfaces to classes

--- src/dto/airport/index.ts
+++ src/dto/airport/index.ts
@@ -1,6 +1,6 @@
 // interfaces
-export { default as IAirportDetail } from './IAirportDetail';
-export { default as IAirportIdentifier } from './IAirportIdentifier';
+// export { default as IAirportDetail } from './IAirportDetail';
+// export { default as IAirportIdentifier } from './IAirportIdentifier';
 
 // classes
 export { default as AirportDetail } from './AirportDetail';
--- src/dto/reservation/index.ts
+++ src/dto/reservation/index.ts
@@ -1,6 +1,9 @@
-export { default as IReservationIdentifier } from './IReservationIdentifier';
-export { default as IReservationDetail } from './IReservationDetail';
-export { default as IReservationSummary } from './IReservationSummary';
+// interfaces
+// export { default as IReservationIdentifier } from './IReservationIdentifier';
+// export { default as IReservationDetail } from './IReservationDetail';
+// export { default as IReservationSummary } from './IReservationSummary';
+
+// classes
 export { default as ReservationDetail } from './ReservationDetail';
 export { default as ReservationIdentifier } from './ReservationIdentifier';
 export { default as ReservationSummary } from './ReservationSummary';
