057fe209981e96731c0a058b9710013f60aa7fcb
Stephan-MDD <stephan.mdd@gmail.com>
2020-11-15 23:17:50 +0100

    change dto from interfaces to classes

--- src/dto/reservation/index.ts
+++ src/dto/reservation/index.ts
@@ -1,9 +1,6 @@
-// interfaces
 export { default as IReservationIdentifier } from './IReservationIdentifier';
 export { default as IReservationDetail } from './IReservationDetail';
 export { default as IReservationSummary } from './IReservationSummary';
-
-// classes
 export { default as ReservationDetail } from './ReservationDetail';
 export { default as ReservationIdentifier } from './ReservationIdentifier';
 export { default as ReservationSummary } from './ReservationSummary';
