a2fad6965cb4b8c4a3d30d3ac843de2b49bcf85d
Stephan-MDD <stephan.mdd@gmail.com>
2020-11-16 10:46:05 +0100

    qiuck fix

--- src/eto/index.ts
+++ src/eto/index.ts
@@ -1,3 +1,25 @@
-export { default as InconsistentLengthError } from './InconsistentLengthError';
-export { default as InvalidInputError } from './InvalidInputError';
-export { default as NotFoundError } from './NotFoundError';
+// export { default as InconsistentLengthErrorClass } from './InconsistentLengthError';
+// export { default as InvalidInputError } from './InvalidInputError';
+// export { default as NotFoundError } from './NotFoundError';
+
+import InconsistentLengthErrorClass from './InconsistentLengthError';
+import InvalidInputErrorClass from './InvalidInputError';
+import NotFoundErrorClass from './NotFoundError';
+
+export class InconsistentLengthError extends InconsistentLengthErrorClass {
+	constructor(msg: string) {
+		super(msg);
+	}
+}
+
+export class InvalidInputError extends InvalidInputErrorClass {
+	constructor(msg: string) {
+		super(msg);
+	}
+}
+
+export class NotFoundError extends NotFoundErrorClass {
+	constructor(msg: string) {
+		super(msg);
+	}
+}
