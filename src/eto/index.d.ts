d8b8ac31e83a429684f6f7f644cccbd241863589
Stephan-MDD <stephan.mdd@gmail.com>
2020-11-16 11:03:10 +0100

    qiuck fix

---
+++ src/eto/index.d.ts
@@ -0,0 +1,3 @@
+export { default as InconsistentLengthErrorClass } from './InconsistentLengthError';
+export { default as InvalidInputError } from './InvalidInputError';
+export { default as NotFoundError } from './NotFoundError';
